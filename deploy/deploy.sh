2b80a63d3b68ddb9a6863f3f5ec99ec54a7e48d2
rramillien <regis@ramillien.fr>
2016-12-04 00:04:28 +0100

    Try deployment with generated settings.xml.

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-if [ "$TRAVIS_BRANCH" == "master" ]; then
-  mvn deploy --settings ./deploy/settings.xml
-fi
+#if [ "$TRAVIS_BRANCH" == "master" ]; then
+  echo "<settings><servers><server><id>ossrh</id><username>\${env.OSSRH_USER}</username><password>\${env.OSSRH_PASS}</password></server></servers></settings>" > ~/settings.xml
+  mvn deploy --settings ~/settings.xml
+#fi
