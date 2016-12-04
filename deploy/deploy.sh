703fc309befff27811c7bf40ad84c22cea5675fe
rramillien <regis@ramillien.fr>
2016-12-04 01:02:59 +0100

    added a forgotten  "$" on TRAVIS_TAG variable.

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ -n "TRAVIS_TAG" ]; then
+if [ -n "$TRAVIS_TAG" ]; then
   echo "<settings><servers><server><id>ossrh</id><username>\${env.OSSRH_USER}</username><password>\${env.OSSRH_PASS}</password></server></servers></settings>" > ~/settings.xml
   mvn deploy --settings ~/settings.xml
 fi
